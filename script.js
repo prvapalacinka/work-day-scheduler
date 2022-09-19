91256a3f05f89d76421acefeaf6c4d4cbf679cd2
Broly <vidovicp20@gmail.com>
2022-09-18 21:14:37 -0400

    added save buttons and formatting

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
 var today = moment ();
 $('#currentDay').text(today.format('LL'));
+
+const rows = document.getElementsByClassName("row");
+let currentHour = parseInt(moment().format('HH:mm'));
+
+Array.from(rows).forEach(row => {
+    let
+        rowIdString = row.id,
+        rowHour;
+    if (rowIdString) {
+        rowHour = parseInt(rowIdString);
+    }
+    if (rowHour) {
+        // Compares row id to current hour and sets color accordingly
+        if (currentHour === rowHour) {
+            setColor(row, "red");
+        } else if ((currentHour < rowHour) && (currentHour > rowHour - 6)) {
+            setColor(row, "green");
+        } else if ((currentHour > rowHour) && (currentHour < rowHour + 6)) {
+            setColor(row, "lightgrey");
+        } else {
+            setColor(row, "white");
+        }
+    }
+});
+
+function setColor(element, color) {
+    element.style.backgroundColor = color;
+}
